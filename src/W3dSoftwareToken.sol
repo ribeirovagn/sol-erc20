67a8b66d68bf56f824a8193215a3496c3703e8b6
Vagner Ribeiro <ribeirovagn@gmail.com>
2022-08-20 23:31:06 -0300

    Test

--- src/W3dSoftwareToken.sol
+++ src/W3dSoftwareToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.16;
 
-import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract W3dSoftwareToken is ERC20 {
-    constructor(uint256 supply) ERC20("W3dSoftwareToken", "W3ST") {
-        _mint(msg.sender, supply);
+    constructor() ERC20("W3d Software Token", "W3ST") {
+        _mint(msg.sender, 10000000);
     }
 }
--- test/W3dSoftwareToken.t.sol
+++ test/W3dSoftwareToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.16;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/W3dSoftwareToken.sol";
 
 contract W3dSoftwareTokenTest is Test {
@@ -10,11 +10,11 @@ contract W3dSoftwareTokenTest is Test {
     address vagner = vm.addr(0x2);
 
     function setUp() public {
-        token = new W3dSoftwareToken(1000);
+        token = new W3dSoftwareToken();
     }
 
     function testTokenName() public {
-        assertEq(token.name(), "W3dSoftwareToken");
+        assertEq(token.name(), "W3d Software Token");
     }
 
     function testTokenSymbol() public {
@@ -22,26 +22,26 @@ contract W3dSoftwareTokenTest is Test {
     }
 
     function testTranfer() public {
-        assertTrue(token.transfer(ribeiro, 500));
-        assertEq(token.balanceOf(ribeiro), 500);
-        assertEq(token.balanceOf(address(this)), 500);
+        assertTrue(token.transfer(ribeiro, 5000000));
+        assertEq(token.balanceOf(ribeiro), 5000000);
+        assertEq(token.balanceOf(address(this)), 5000000);
     }
 
     function testAprove() public {
-        assertTrue(token.approve(ribeiro, 500));
+        assertTrue(token.approve(ribeiro, 5000000));
     }
 
     function testAllowance() public {
-        assertTrue(token.approve(ribeiro, 500));
-        assertEq(token.allowance(address(this), ribeiro), 500);
+        assertTrue(token.approve(ribeiro, 5000000));
+        assertEq(token.allowance(address(this), ribeiro), 5000000);
     }
 
     function testTranferFrom() public {
-        assertTrue(token.approve(ribeiro, 500));
-        assertEq(token.allowance(address(this), ribeiro), 500);
+        assertTrue(token.approve(ribeiro, 5000000));
+        assertEq(token.allowance(address(this), ribeiro), 5000000);
         vm.prank(ribeiro);
-        assertTrue(token.transferFrom(address(this), vagner, 500));
-        assertEq(token.balanceOf(vagner), 500);
+        assertTrue(token.transferFrom(address(this), vagner, 5000000));
+        assertEq(token.balanceOf(vagner), 5000000);
         vm.stopPrank();
     }
 }
